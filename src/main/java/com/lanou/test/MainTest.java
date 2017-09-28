b85d0d62e3bc95287ecdf12a8d8848c8ec3be8b3
zar <a18502436094@163.com>
2017-09-28 15:18:05 +0800

    第二次提交,改变了文件,在MainTest文件中添加了5行输出语句

--- src/main/java/com/lanou/test/MainTest.java
+++ src/main/java/com/lanou/test/MainTest.java
@@ -5,7 +5,14 @@ package com.lanou.test;
  */
 public class MainTest {
     public static void main(String[] args) {
-
+        System.out.println("孙钰博");
+        System.out.println("孙钰博");
+        System.out.println("孙钰博");
+        System.out.println("孙钰博");
+        System.out.println("孙钰博");
+        System.out.println("孙钰博");
+        System.out.println("孙钰博");
+        System.out.println("孙钰博");
         System.out.println("别睡觉,会死!");
     }
 }
