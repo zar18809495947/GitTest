5deb9b614b8a32b467cef49a196c5504155565d9
zar <a18502436094@163.com>
2017-09-28 15:13:27 +0800

    第一次提交,添加工具文件,完成了所有的工作

---
+++ src/main/java/com/lanou/test/MainTest.java
@@ -0,0 +1,11 @@
+package com.lanou.test;
+
+/**
+ * Created by zar on 2017/9/28.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+
+        System.out.println("别睡觉,会死!");
+    }
+}
